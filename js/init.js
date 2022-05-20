96f764530ec9eec3fe328415f2899809b160716e
alglus <60944888+alglus@users.noreply.github.com>
2022-05-20 17:37:39 +0200

    Replaced default grammar

--- js/init.js
+++ js/init.js
@@ -6,7 +6,7 @@ export let grammarInputField;
 $(document).ready(function () {
 
     // Check if it is a mobile device and show a warning.
-    if (window.matchMedia( "(hover: none)" ).matches) {
+    if (window.matchMedia("(hover: none)").matches) {
         // hover unavailable
         $('#mobileWarning').css('display', 'flex');
     }
@@ -36,11 +36,9 @@ $(document).ready(function () {
     grammarInputField = CodeMirror(document.getElementById('grammarInputWrapper'), {
         lineNumbers: true,
         lineWrapping: true,
-        value: 'S -> A   : z[0] = z[1]; c[1] = 0\n' +
-            'A -> s B : a[2] = y[2]; b[2] = c[0]; z[0] = x[2]\n' +
-            'A -> t B : a[2] = c[0]; b[2] = x[2]; z[0] = y[2]\n' +
-            'B -> u   : x[0] = a[0]; y[0] = b[0]\n' +
-            'B -> v   : y[0] = x[0]; x[0] = 0'
+        value: 'S -> L : h[0] = h[1]; i[1] = j[1]; j[0] = j[1]; k[1] = h[1]; i[0] = 0; k[0] = 0\n' +
+            'L -> a : j[0] = k[0]; h[0] = 0; i[0] = 0\n' +
+            'L -> b : h[0] = i[0]; j[0] = 0; k[0] = 0'
     });
 
 
